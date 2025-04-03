f519f63c46a6868df81cc8179b73550c2b7023c7
Ariel <arieltwain@gmail.com>
2025-04-03 13:45:04 -0300

    finishing tests for sign up page, deleting unused files and starting the global auth setup config

--- playwright/tests/signUpPage.spec.ts
+++ playwright/tests/signUpPage.spec.ts
@@ -18,6 +18,8 @@ test.describe("SignUpPage", (): void => {
         console.log('Before tests');
         signUpPage = new SignUpPage(page);
         await signUpPage.goTo();
+        //TODO
+        // Add a more sophisticated way to handle test data
         data = JSON.parse(fs.readFileSync(`./playwright/test-data/users.json`, 'utf-8'));
     });
 
@@ -43,8 +45,7 @@ test.describe("SignUpPage", (): void => {
      test('Verify that user is not able to Sign Up with an already registered Email', async ({ page }) => {
 
          // Arrange
-
-         // Create a user
+         // Create a user and sign up
          const username = faker.internet.username();
          const email = faker.internet.email();
          const password = faker.internet.password();
@@ -65,6 +66,7 @@ test.describe("SignUpPage", (): void => {
          // Click Logout button
          await page.getByRole('button').last().click();
 
+
          // Act
          // Try to crete the same user
          await signUpPage.goTo();
@@ -72,14 +74,15 @@ test.describe("SignUpPage", (): void => {
          await signUpPage.clickSignUpButton();
 
          // Assert
+         // Verify that the correct message is displayed
          expect(await signUpPage.getEmailErrorMessage()).toContain("email has already been taken");
 
      });
 
      test('Verify that User is not able to Sign Up with an already registered Username', async ({ page }) => {
-         // Arrange
 
-         // Create a user
+         // Arrange
+         // Create a user and sign up
          const username = faker.internet.username();
          const email = faker.internet.email();
          const password = faker.internet.password();
@@ -107,10 +110,51 @@ test.describe("SignUpPage", (): void => {
          await signUpPage.clickSignUpButton();
 
          // Assert
+         // Verify that the correct message is displayed
          expect(await signUpPage.getUsernameErrorMessage()).toContain("username has already been taken");
 
      });
-// test('Verify that User is not able to Sign Up with an invalid email format', async ({ page }) => {});
-// test('Verify that User is not able to Sign Up with a short password', async ({ page }) => {});
+
+     // This test should fail
+     test('Verify that User is not able to Sign Up with an invalid email format', async ({ page }) => {
+
+         // Arrange
+         // Create a user and fill sign up form
+         const username = faker.internet.username();
+         const email = "invalidemailformat";
+         const password = faker.internet.password();
+
+         await signUpPage.fillSignUpForm(username, email, password);
+
+         // Act
+         // Click sign up button
+         await signUpPage.clickSignUpButton();
+
+         // Assert
+         // Verify that user is not able to sign up
+         expect(page.url()).not.toContain('/register');
+
+     });
+
+     test('Verify that User is not able to Sign Up with a short password', async ({ page }) => {
+
+         // Arrange
+         // Create a user and fill sign up form
+         const username = faker.internet.username();
+         const email = faker.internet.email();
+         const password = "pass";
+
+         await signUpPage.fillSignUpForm(username, email, password);
+
+         // Act
+         // Click sign up button
+         await signUpPage.clickSignUpButton();
+
+         // Assert
+         // Verify that user is not able to sign up
+         expect(await signUpPage.getUsernameErrorMessage()).toContain("password should have at least 8 characters");
+         expect(page.url()).not.toContain('/register');
+
+     });
 
 });
